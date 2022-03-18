9de87e2e85b1bbe17410ef0c5adaf1774733c190
Krzysztof <krzubrycki@gmail.com>
2022-03-18 21:25:11 +0100

    created project

---
+++ src/main/java/com/javastart/JavastartSpringMvcExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartSpringMvcExcerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartSpringMvcExcerciseApplication.class, args);
+    }
+
+}
