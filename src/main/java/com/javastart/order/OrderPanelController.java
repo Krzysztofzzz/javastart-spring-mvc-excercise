c4d10217c72743dc366a551871f6a17a10cb855f
Krzysztof <krzubrycki@gmail.com>
2022-03-21 16:32:55 +0100

    added orders progress options

--- src/main/java/com/javastart/order/OrderPanelController.java
+++ src/main/java/com/javastart/order/OrderPanelController.java
@@ -1,7 +1,28 @@
 package com.javastart.order;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class OrderPanelController {
+    private final OrderRepository orderRepository;
+
+    public OrderPanelController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping("/panel/zamowienia")
+    public String getOrders(@RequestParam(required = false) OrderStatus status, Model model) {
+        List<Order> orders;
+        if (status == null)
+            orders = orderRepository.findAll();
+        else
+            orders = orderRepository.findAllByStatus(status);
+        model.addAttribute("orders", orders);
+        return "panel/orders";
+    }
 }
